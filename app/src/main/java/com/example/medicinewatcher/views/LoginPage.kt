015c092588fabd238197b2781eb23c9e4fc916d8
Can Ersoy <can.ersoy@tum.de>
2023-08-14 17:19:31 +0300

    firebase setup

---
+++ app/src/main/java/com/example/medicinewatcher/views/LoginPage.kt
@@ -0,0 +1,15 @@
+package com.example.medicinewatcher.views
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class LoginPage {
+
+     @Preview
+     @Composable
+     fun LoginPage(){
+
+     }
+
+
+}
